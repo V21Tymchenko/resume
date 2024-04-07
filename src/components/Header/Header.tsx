5215a0c4591b507bde724d574644357422bc22dd
Valeriia tymchenko <lera.tymchenko@ukr.net>
2024-04-07 15:32:58 +0300

    base text in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,32 @@
 import Navigation from "../Navigation";
+import { IoIosMore } from "react-icons/io";
+import { LiaTelegramPlane } from "react-icons/lia";
+import { useToggleOpen } from "@/helpers/hooks";
+import css from "./Header.module.css";
 
 const Header = () => {
+  const { isOpen, toggle } = useToggleOpen(false);
+
   return (
-    <>
-      <Navigation />
-    </>
+    <header className={css.header}>
+      <button type="button" onClick={toggle} className={css.navigationIcon}>
+        <IoIosMore className={`${isOpen ? css.rotated : ""}`} />
+        <span>Навігація сайтом</span>
+      </button>
+      <div className={css.languageContainer}>
+        <button type="button">UK</button>
+        <button type="button">EN</button>
+      </div>
+      <a
+        href="tg://resolve?domain=@valerytymchenko"
+        target="_blank"
+        className={css.navigationIcon}
+      >
+        <span> Valeriia Tymchenko</span>
+        <LiaTelegramPlane />
+      </a>
+      {isOpen && <Navigation />}
+    </header>
   );
 };
 export default Header;
